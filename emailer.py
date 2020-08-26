f9991227fb6803bf3c2153732d11a4f33d28295d
Felix Siu <46659537+felixwsiu@users.noreply.github.com>
2020-08-25 23:49:11 -0700

    Initial Commit for Email Service
    WHAT:
    --------------
    Establishes a secure TLS encrypted connection to send emails using an existing gmail
    
    HOW:
    --------------
    A SMPTP Connection will be secured from the beginning using SMPTP_SSL(), Sender, Receiver and authentication information is needed to successfully send an email.

---
+++ emailer.py
@@ -0,0 +1,31 @@
+import smtplib, ssl
+
+# A SMPTP Connection will be secured from the beginning using SMTP_SSL()
+# For this code to work, your google account must have "Allow Less Secure Apps to ON"
+# or your login will be deemed dangerous. (WARNING : This will make your account more vulnerable)
+# TLS-encrypted connection will be initiated, and the default context of SSL will load the system’s trusted CA certificates,
+# enable host name checking and certificate validation, and try to choose reasonably secure protocol and cipher settings.
+
+port = 465 
+smtp_server = "smtp.gmail.com"
+
+# Email Settings
+sender_email = "getyourubccourse@gmail.com" 
+receiver_email = "your@gmail.com"  
+
+message = """\
+Subject: Hi there
+
+This message is sent from Python.
+"""
+
+
+# EMAILPASS is an ENV on heroku's servers. If you want to run this locally, you can simply replace it with
+# an input("")
+password = EMAILPASS; #input("Type your password and press enter: ")
+
+def sendEmailTest():
+	context = ssl.create_default_context()
+	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+	    server.login(sender_email, password)
+	    server.sendmail(sender_email, receiver_email, message)
