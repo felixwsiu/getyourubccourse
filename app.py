9a414883974ab4823e4315d4713e7d0799e5010a
Felix Siu <46659537+felixwsiu@users.noreply.github.com>
2020-08-26 01:25:19 -0700

    Experimenting with threading

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template
 from scheduler import turnOnScheduler
 from threading import Thread
 import emailer
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+	return render_template("home.html")
 
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
-
+	return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-	Thread(target=turnOnScheduler()).start()
-	Thread(target=emailer.sendEmailTest()).start()
-	app.run(debug=True)
-	
+	Thread(turnOnScheduler()).start()
+	Thread(emailer.sendEmailTest()).start()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port, threaded=True, debug=True)
