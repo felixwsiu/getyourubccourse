7bb340bfb7e243fb68227de85a0f3e0a6bfe57e5
Felix Siu <46659537+felixwsiu@users.noreply.github.com>
2020-08-29 20:03:42 -0700

    Created a CourseRequest class

---
+++ CourseRequest.py
@@ -0,0 +1,16 @@
+# A CourseRequest that is submitted from a user trying to get into a course
+# Attributes:
+# {string} dept : department of the course (eg. CPSC)
+# {string} course : course number (eg. 201)
+# {string} section : section number (eg. 001)
+# {email} email : email of the user
+
+class CourseRequest:
+
+	def __init__(self, dept, course, section, email):
+		self.dept = dept
+		self.course = course
+		self.section = section
+		self.email = email
+
+
