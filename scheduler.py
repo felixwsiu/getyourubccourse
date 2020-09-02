a8dc77c19e194b1ff3252c34349315f43106d636
Felix Siu <46659537+felixwsiu@users.noreply.github.com>
2020-09-02 16:21:07 -0700

    Finished scheduler functionality
    Will check if course seats are open before sending the proper email.
    Each course request in the list will have its date checked to see if it has expired to prevent overloading the DB. (Currently set at a maximum of 30 days)

--- scheduler.py
+++ scheduler.py
@@ -1,20 +1,28 @@
-import time
-import atexit
-from apscheduler.schedulers.background import BackgroundScheduler
-
-# Turns on a scheduler that is used to iterate over the course request list on a given interval
-# Scheduler will shut down upon exit
-def turnOnScheduler():
-	print("Scheduler is now turning on")
-	scheduler = BackgroundScheduler()
-	#Interation frequency can be changed here
-	scheduler.add_job(func=checkCoursesForUsers, trigger="interval", minutes=0.05)
-	scheduler.start()
-	atexit.register(lambda: scheduler.shutdown())
+from datetime import date, datetime
+import databaseutil 
+import coursescraper
+import emailer
+#Scheduler.py will be run on Heroku Scheduler with 10 minute intervals
 
 
-# Runs on a given interval to iterate through the user list and notify users
+# Runs on a given interval to iterate through the list of course requests and notify users
 # if a spot has opened up in their courses
 def checkCoursesForUsers():
-    print("Current Time: " + time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+	requests = databaseutil.getAllRequests()
+	# over due requests will be deleted to clean up DB
+	delete = []
+	for r in requests:
+		if coursescraper.isCourseSeatOpen(r.dept, r.course, r.section):
+			emailer.sendCourseSeatEmail(r.email, r.dept, r.course, r.section)
+		requestExpiryCheck(r.id, r.email, r.dateAdded)
+		
 
+# The dateAdded string will be converted to the date type to be compared to todays date
+# If it is greater than 30 (1 month), the request will expire and be deleted
+# @params {string} id: unique id name for the representing request
+# @params {string} email: user's email that is also used as the partition key for this container
+def requestExpiryCheck(id, email, dateAdded):
+	if (date.today() - datetime.strptime(dateAdded, "%Y-%m-%d").date()).days > 30:
+		databaseutil.deleteRequest(id,email)
+
+checkCoursesForUsers()
