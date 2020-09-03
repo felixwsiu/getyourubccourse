54b4b2f77b5fadeed7864160ae5fcdf7523d5ac5
Felix Siu <46659537+felixwsiu@users.noreply.github.com>
2020-09-02 22:58:55 -0700

    Revamped home page UI and CSS
    Entire make over of front page with Bootstrap and working WTF Forms.
    Alerts are added depending on form validation

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
+
+class courseRequestForm(FlaskForm):
+	email = StringField("Your Email", validators=[DataRequired(),Email("Email is invalid.")])
+	department = StringField("Course Department", validators=[DataRequired()])
+	course = StringField("Course Number", validators=[DataRequired()])
+	section = StringField("Section Number", validators=[DataRequired()])
+	register = SubmitField("Register")
